e1748b3eae878c7015c5f7a4b774ee739cc6d45e
Kriptikz <Kriptikz@users.noreply.github.com>
2021-07-05 14:04:22 -0500

    reserved 99 in last two digits of dna as a kitty gene

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -23,7 +23,7 @@ contract ZombieFeeding is ZombieFactory
     address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
     KittyInterface kittyContract = KittyInterface(ckAddress);
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna) public
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public
     {
         require(msg.sender == zombieToOwner[_zombieId]);
 
@@ -33,6 +33,11 @@ contract ZombieFeeding is ZombieFactory
 
         uint newDna = (myZombie.dna + _targetDna) / 2;
 
+        if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty")))
+        {
+            newDna = newDna - newDna % 100 + 99;
+        }
+
         _createZombie("NoName", newDna);
     }
 
@@ -41,6 +46,6 @@ contract ZombieFeeding is ZombieFactory
         uint kittyDna;
         (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
 
-        feedAndMultiply(_zombieId, kittyDna);
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
     }
 }
