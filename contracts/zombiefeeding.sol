7f0d8a302992d08e14950c2916eb8511ac5c9e7a
Kriptikz <Kriptikz@users.noreply.github.com>
2021-07-24 11:08:12 -0500

    Implemented function _isReady to check if our zombie cooldown is finished

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -32,6 +32,11 @@ contract ZombieFeeding is ZombieFactory
         _zombie.readyTime = uint32(now + cooldownTime);
     }
 
+    function _isReady(Zombie storage _zombie) internal view returns (bool)
+    {
+        return (_zombie.readyTime <= now);
+    }
+
     function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public
     {
         require(msg.sender == zombieToOwner[_zombieId]);
