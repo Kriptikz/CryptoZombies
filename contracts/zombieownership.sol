749a894498afc040e8c7ab99ca4ec7f802457a40
Kriptikz <Kriptikz@users.noreply.github.com>
2021-07-28 13:26:11 -0500

    added function stubs in ZombieOwnership for ERC721 interface

--- contracts/zombieownership.sol
+++ contracts/zombieownership.sol
@@ -5,5 +5,23 @@ import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721
 {
+  function balanceOf(address _owner) external view returns (uint256)
+  {
 
+  }
+
+  function ownerOf(uint256 _tokenId) external view returns (address)
+  {
+
+  }
+
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable
+  {
+
+  }
+
+  function approve(address _approved, uint256 _tokenId) external payable
+  {
+
+  }
 }
