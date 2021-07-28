a337372209a357cfde64aa7e6c307be392a08544
Kriptikz <Kriptikz@users.noreply.github.com>
2021-07-28 14:41:20 -0500

    Refactor ZombieAttack to use SafeMath

--- contracts/zombieattack.sol
+++ contracts/zombieattack.sol
@@ -9,7 +9,7 @@ contract ZombieAttack is ZombieHelper
 
     function randMod(uint _modulus) internal returns (uint)
     {
-        randNonce++;
+        randNonce = randNonce.add(1);
         return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
     }
 
@@ -22,15 +22,15 @@ contract ZombieAttack is ZombieHelper
 
         if (rand <= attackVictoryProbability)
         {
-            myZombie.winCount++;
-            myZombie.level++;
-            enemyZombie.lossCount++;
+            myZombie.winCount = myZombie.winCount.add(1);
+            myZombie.level = myZombie.level.add(1);
+            enemyZombie.lossCount = enemyZombie.lossCount.add(1);
             feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
         }
         else
         {
-            myZombie.lossCount++;
-            enemyZombie.winCount++;
+            myZombie.lossCount = myZombie.lossCount.add(1);
+            enemyZombie.winCount = enemyZombie.winCount.add(1);
             _triggerCooldown(myZombie);
         }
     }
