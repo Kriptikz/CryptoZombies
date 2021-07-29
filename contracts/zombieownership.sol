26ef3ad7a77155ba388cf0854bdcd66549edce64
Kriptikz <Kriptikz@users.noreply.github.com>
2021-07-29 13:13:26 -0500

    Added natspec tags to ZombieOwnership contract

--- contracts/zombieownership.sol
+++ contracts/zombieownership.sol
@@ -3,6 +3,9 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombieattack.sol";
 import "./erc721.sol";
 
+/// @title A contract for basic zombie ownership using the ERC721 standard
+/// @author Kriptikz
+/// @dev Compliant with OpenZeppelin's implementation of the ERC721 spec draft
 contract ZombieOwnership is ZombieAttack, ERC721
 {
     mapping (uint => address) zombieApprovals;
